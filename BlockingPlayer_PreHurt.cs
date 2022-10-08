34a54a36ce60625b5e3b88811eac71df1cddca27
Antinoe <88167211+Antinoe@users.noreply.github.com>
2022-10-08 15:16:03 -0400

    PreHurt Fix
    Before this patch, damage would continuously be received when Guarding against Lava, for example. With this, the Parrying and Guarding code cannot run until ``Player.immuneTime`` is 0.

--- BlockingPlayer_PreHurt.cs
+++ BlockingPlayer_PreHurt.cs
@@ -21,82 +21,89 @@ namespace Blocking
 		//Right before receiving damage.
 		public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource, ref int cooldownCounter)
         {
-			if (parryTimer >= 1) //Parrying.
+			if (Player.immuneTime > 0)
 			{
-				if (BlockingConfigClient.Instance.enableSoundsParrying)
-				{
-					SoundEngine.PlaySound(Parry, Player.position);
-				}
-				if (BlockingConfigClient.Instance.enableScreenshakeParrying)
-				{
-					screenShakeTimerParrying += BlockingConfig.Instance.parryTimer;
-				}
-				Player.immune = true;
-				Player.immuneTime = BlockingConfig.Instance.parryImmuneTime; //Parry Immune Time is set to the configured value.
-				parryCooldown = BlockingConfig.Instance.parryCooldown;
-				if (parryCounterCooldown == 0 && BlockingConfig.Instance.enableParryCounters)
-				{
-					Player.AddBuff(BuffID.ParryDamageBuff, BlockingConfig.Instance.parryCounterTime); //Add the Striking Moment buff.
-					parryCounterCooldown = BlockingConfig.Instance.parryCounterCooldown;
-				}
-				return false; //The reason we put the return value here, at the end of the Parry Counter code, rather than at the end of the Parry code, is because nothing after a Return statement is run. Therefore, if we had done the aforementioned, the Parry Counter code wouldn't run at all, rendering the ability useless.
+				return false;
 			}
-			
-			if (guardTimer >= 1 && !hasShield && parryTimer == 0) //Guarding, but not Parrying and not Shield Guarding.
+			else
 			{
-				//The 2 strings below are what cause the Player to be launched back upon Shield Guarding.
-				Player.velocity.X = 5f * hitDirection;
-				Player.velocity.Y = -3f;
-				if (BlockingConfigClient.Instance.enableSoundsGuardingBlock)
+				if (parryTimer >= 1) //Parrying.
 				{
-					SoundEngine.PlaySound(Block, Player.position);
-				}
-				guardingCooldown = BlockingConfig.Instance.guardingCooldown;
-				if (potentGuarding && canPotentGuard && !BlockingConfig.Instance.potentGuardingRequiresShield && Player.statMana >= damage - Player.statDefense)
-				{
-					Player.statMana -= damage - Player.statDefense;
+					if (BlockingConfigClient.Instance.enableSoundsParrying)
+					{
+						SoundEngine.PlaySound(Parry, Player.position);
+					}
+					if (BlockingConfigClient.Instance.enableScreenshakeParrying)
+					{
+						screenShakeTimerParrying += BlockingConfig.Instance.parryTimer;
+					}
 					Player.immune = true;
-					Player.immuneTime = BlockingConfig.Instance.blockingImmuneTime;
-					return false;
+					Player.immuneTime = BlockingConfig.Instance.parryImmuneTime; //Parry Immune Time is set to the configured value.
+					parryCooldown = BlockingConfig.Instance.parryCooldown;
+					if (parryCounterCooldown == 0 && BlockingConfig.Instance.enableParryCounters)
+					{
+						Player.AddBuff(BuffID.ParryDamageBuff, BlockingConfig.Instance.parryCounterTime); //Add the Striking Moment buff.
+						parryCounterCooldown = BlockingConfig.Instance.parryCounterCooldown;
+					}
+					return false; //The reason we put the return value here, at the end of the Parry Counter code, rather than at the end of the Parry code, is because nothing after a Return statement is run. Therefore, if we had done the aforementioned, the Parry Counter code wouldn't run at all, rendering the ability useless.
 				}
-				else //No Mana to consume.
+				
+				if (guardTimer >= 1 && !hasShield && parryTimer == 0) //Guarding, but not Parrying and not Shield Guarding.
 				{
-					return true;
-				}
-			}
-			
-			if (guardTimer >= 1 && hasShield && parryTimer == 0) //Shield Guarding, but not Parrying.
-			{
-				//The 2 strings below are what cause the Player to be launched back upon Shield Guarding.
-				Player.velocity.X = 5f * hitDirection;
-				Player.velocity.Y = -3f;
-				if (potentGuarding && canPotentGuard && Player.statMana >= damage - Player.statDefense)
-				{
-					Player.statMana -= damage - Player.statDefense;
-				if (BlockingConfigClient.Instance.enableSoundsGuardingBlockShield)
-				{
-					SoundEngine.PlaySound(BlockShield, Player.position);
-				}
-					Player.immune = true;
-					Player.immuneTime = BlockingConfig.Instance.blockingImmuneTime;
-					potentGuardingCooldown = BlockingConfig.Instance.potentGuardingCooldown;
-					return false;
-				}
-				else //No Mana to consume.
-				{
+					//The 2 strings below are what cause the Player to be launched back upon Shield Guarding.
+					Player.velocity.X = 5f * hitDirection;
+					Player.velocity.Y = -3f;
 					if (BlockingConfigClient.Instance.enableSoundsGuardingBlock)
 					{
 						SoundEngine.PlaySound(Block, Player.position);
 					}
-					if (BlockingConfigClient.Instance.enableSoundsGuardingBlockShieldBroken)
-					{
-						SoundEngine.PlaySound(BlockShieldBroken, Player.position);
-					}
 					guardingCooldown = BlockingConfig.Instance.guardingCooldown;
-					return true;
+					if (potentGuarding && canPotentGuard && !BlockingConfig.Instance.potentGuardingRequiresShield && Player.statMana >= damage - Player.statDefense)
+					{
+						Player.statMana -= damage - Player.statDefense;
+						Player.immune = true;
+						Player.immuneTime = BlockingConfig.Instance.blockingImmuneTime;
+						return false;
+					}
+					else //No Mana to consume.
+					{
+						return true;
+					}
 				}
+				
+				if (guardTimer >= 1 && hasShield && parryTimer == 0) //Shield Guarding, but not Parrying.
+				{
+					//The 2 strings below are what cause the Player to be launched back upon Shield Guarding.
+					Player.velocity.X = 5f * hitDirection;
+					Player.velocity.Y = -3f;
+					if (potentGuarding && canPotentGuard && Player.statMana >= damage - Player.statDefense)
+					{
+						Player.statMana -= damage - Player.statDefense;
+					if (BlockingConfigClient.Instance.enableSoundsGuardingBlockShield)
+					{
+						SoundEngine.PlaySound(BlockShield, Player.position);
+					}
+						Player.immune = true;
+						Player.immuneTime = BlockingConfig.Instance.blockingImmuneTime;
+						potentGuardingCooldown = BlockingConfig.Instance.potentGuardingCooldown;
+						return false;
+					}
+					else //No Mana to consume.
+					{
+						if (BlockingConfigClient.Instance.enableSoundsGuardingBlock)
+						{
+							SoundEngine.PlaySound(Block, Player.position);
+						}
+						if (BlockingConfigClient.Instance.enableSoundsGuardingBlockShieldBroken)
+						{
+							SoundEngine.PlaySound(BlockShieldBroken, Player.position);
+						}
+						guardingCooldown = BlockingConfig.Instance.guardingCooldown;
+						return true;
+					}
+				}
+				return true; //Keep this here. It must be at the end of the PreHurt Method so that the Player takes damage by default.
 			}
-			return true; //Keep this here. It must be at the end of the PreHurt Method so that the Player takes damage by default.
 		}
 	}
 }
